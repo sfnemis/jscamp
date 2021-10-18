9811524ab1e87c57f536a6fd679a290871660506
SFN <sfnemis@gmail.com>
2021-10-19 00:43:12 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,33 @@
+console.log("Merhaba Kodlama.io")
+
+//JS type safe degildir. 
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+
+let konutKredileri = ["Konut kredisi",'Emlak Konut Kredisi','Kamu Konut Kredisi','Dinamize Ettim']
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+console.log("<li>" +konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
